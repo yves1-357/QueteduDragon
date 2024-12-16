00c604d6e1175d62e583548fe38b93f880e71c15
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2024-12-16 16:22:34 +0100

    creation de la classe Thief.cs

---
+++ QueteDuDragon/Data/Hero/Thief.cs
@@ -0,0 +1,13 @@
+﻿namespace QueteDuDragon.Data.Hero;
+
+public class Thief : Hero
+    {
+        public Thief(): base("Voleur"){}
+        public override void InitializeSkills()
+        {
+            Skills.Add("Flèche empoisonnée");
+            Skills.Add("Piège");
+            Skills.Add("Disparition");
+        }
+    }
+}
