8b814bc5eebe5a459d762655ef794730b58460cf
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2024-12-16 17:26:02 +0100

    creation de la class bossFinal

---
+++ QueteDuDragon/Data/bossFinal/Dragon.cs
@@ -0,0 +1,16 @@
+﻿namespace QueteDuDragon.Data.bossFinal;
+using QueteDuDragon.Data.Heroes;
+
+
+public class Dragon
+{ 
+    public int pointsVie { get; set; } = 10;
+        public int AttackPower { get; set; } = 1;
+
+        public bool isDefeated => pointsVie <= 0;
+
+        public void Attack(Hero hero)
+        {
+            hero.pointsVie -= AttackPower;
+        }
+    }
