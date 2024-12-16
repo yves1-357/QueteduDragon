c6249fa3b47a794500929c367dc56021841b6a5f
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2024-12-16 16:21:25 +0100

    creation des premieres classes

--- QueteDuDragon/Data/Hero/hero.cs
+++ QueteDuDragon/Data/Hero/hero.cs
@@ -1,6 +1,38 @@
-﻿namespace QueteDuDragon.Data.Hero;
-
-public class hero
+﻿namespace QuêteduDragon.Data.Hero
 {
-    
+    public abstract class Hero
+    {
+        public string Name { get; set; }
+        public int pointsVie { get; set; } = 5;
+        public List<string> Skills { get; set; }
+        public List<string> objetsCollectes { get; set; } = new List<string>();
+        public int niveauExperience { get; set; } = 1;
+
+        public Hero(string name)
+        {
+            Name = name;
+            
+        }
+
+        public void objetsCollecte(string objets)
+        {
+            objetsCollectes.Add(objets);
+            UpdateniveauExperience();
+        }
+
+        private void UpdateniveauExperience()
+        {
+            int objetsCount = objetsCollectes.Count;
+            if (objetsCount >= 7) niveauExperience = 3;
+            else if (objetsCount >= 4) niveauExperience = 2;
+        }
+        
+        public virtual void InitializeSkills()
+        {
+            // Vide par défaut
+        }
+
+    }
+
 }
+
