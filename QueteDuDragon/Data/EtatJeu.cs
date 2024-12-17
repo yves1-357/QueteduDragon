5273bd6b96266ce02d73285f3232705bd818b340
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2024-12-17 12:37:51 +0100

    gestion des heros dans le jeu

---
+++ QueteDuDragon/Data/EtatJeu.cs
@@ -0,0 +1,13 @@
+﻿namespace QueteDuDragon.Data;
+
+using QueteDuDragon.Data.Heroes;
+
+public class EtatJeu
+{
+    public Hero? SelectedHero { get; set; }
+
+    public void SelectHero(Hero hero)
+    {
+        SelectedHero = hero; 
+    }
+}
