9a62c0b3cea180fc9a9664285ef170edc63e5068
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2024-12-20 11:27:51 +0100

    ajout de class test2

--- QueteDuDragon/Data/Heroes/Hero.cs
+++ QueteDuDragon/Data/Heroes/Hero.cs
@@ -5,6 +5,8 @@
         public string Name { get; set; }
         public int pointsVie { get; set; } = 5;
         public List<string> Skills { get; set; } = new List<string>();
+
+        public List<string> Tools { get; set; } = new List<string>(); // outils achete au magasin 
         public List<string> objetsCollectes { get; set; } = new List<string>();
         public int niveauExperience { get; set; } = 1;
 
@@ -30,6 +32,24 @@
         public virtual void InitializeSkills()
         {
             // Vide par défaut
+        }
+
+        public int UseSkill(string skill)
+        {
+            Random random = new Random();
+            return random.Next(10, 31);
+        }
+
+        public int UseTool(string tool)
+        {
+            Random random = new Random();
+            return random.Next(5, 21);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            pointsVie -= damage;
+            if (pointsVie < 0) pointsVie = 0;
         }
         
     }
