41a935eb4d184641dedb2a75316ba13035063601
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2025-01-14 21:26:04 +0100

    rajout image

--- QueteDuDragon/Data/Heroes/Hero.cs
+++ QueteDuDragon/Data/Heroes/Hero.cs
@@ -6,15 +6,17 @@
         public int pointsVie { get; set; } = 5;
         public int Level { get; set; } = 1; // Par défaut, le héros commence au niveau 1
         public List<string> Skills { get; set; } = new List<string>();
+        public string ImageName { get; set; }
 
         public List<string> Tools { get; set; } = new List<string>(); // outils achete au magasin 
         public List<string> objetsCollectes { get; set; } = new List<string>();
         public int niveauExperience { get; set; } = 1;
 
-        public Hero(string name)
+        public Hero(string name, string imageName)
         {
             Name = name;
-            
+            ImageName = imageName;
+
         }
 
         public void AjouterObjet(string objets)
